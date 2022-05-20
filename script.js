e17e73512f9dcfd19dee44804018f1495a1a9f89
Abdul Samad <abdulsamad.laghari1@gmail.com>
2022-05-20 08:39:59 +0500

    cube mid

--- script.js
+++ script.js
@@ -15,20 +15,18 @@ slideshowDivs();
 
 const divs = document.querySelectorAll(".slideshow div");
 let a = 1;
-const slideshow = () => {
-  const div = document.querySelector(".change");
-
+const slidshow = () => {
   setInterval(() => {
     a++;
+    const div = document.querySelector(".slideshow .change");
     div.classList.remove("change");
-
     if (a < divs.length) {
       div.nextElementSibling.classList.add("change");
     } else {
       divs[0].classList.add("change");
       a = 1;
     }
-  }, 1000);
+  }, 10000);
 };
 
-slideshow();
+slidshow();
